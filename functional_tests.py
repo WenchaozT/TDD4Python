676434aaf90a373dcc4b142e0eb089fc1fa3957f
wenchaoz <wenchaoz.t@outlook.com>
2018-09-09 00:19:22 +0800

    输入待办事项功能测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,15 +18,28 @@ class NewVistorTest(unittest.TestCase):
         #T想开发一个todo-list,这是首页
         self.browser.get('http://localhost:8000')
 
-        #检查网页标题
+        #检查网页标题和头部是否都包含Todo
         self.assertIn('Todo', self.browser.title) 
-        # self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Todo', header_text)
 
         #应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox, getattr('placeholder'),
+            'Enter a to-do item'
+        )
 
         #T输入了“学习django”
+        inputbox.send_keys('learn django')
 
         #T按回车键后，页面更新，待办事项表格中新增“1：学习django”
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: learn django' for row in rows)
+        )
 
         #页面中仍有文本框，并可以继续输入
         #T输入了“实践使用selenium”
