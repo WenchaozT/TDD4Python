326d5ffaf800a4deb2dbfee3a9d8abb9737e97ea
wenchaoz <wenchaoz.t@outlook.com>
2018-09-11 00:13:16 +0800

    new func check_td_in_tr in functional_test.py

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_td_in_tr(self, exp_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(exp_text, [row.text for row in rows])
+
     def test4start_a_todolist(self):
         #T想开发一个todo-list,这是首页
         self.browser.get('http://localhost:8000')
@@ -35,17 +40,17 @@ class NewVistorTest(unittest.TestCase):
 
         #T按回车键后，页面更新，待办事项表格中新增“1：学习django”
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: learn django' for row in rows),
-            "New item did't appear in table"
-        )
+        self.check_td_in_tr("1: learn django")
 
         #页面中仍有文本框，并可以继续输入
         #T输入了“实践使用selenium”
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('learn selenium')
+        inputbox.send_keys(Keys.ENTER)
 
-        #页面再次更新，显示了上述两个事项
+        #页面再次更新，显示了上述两个事项  
+        self.check_td_in_tr("1: learn django")
+        self.check_td_in_tr("2: learn selenium")
 
         #T要确定该网站是否保存他的清单
         #网站生成了一个URL,并配有相关说明
