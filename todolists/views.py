42ec1f0b95f9d924d500c7a1dc379989756c059c
wenchaoz <wenchaoz.t@outlook.com>
2019-08-07 15:11:30 +0800

    返回Json；新增删除todoList接口；新增删除todoItem接口

--- todolists/views.py
+++ todolists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.http import JsonResponse
 
 from todolists.models import Item, List
 
@@ -9,7 +10,7 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    # items = Item.objects.filter(list=list_)
     return render(request, "todolists/todolists.html", {'list': list_})
 
 
@@ -18,7 +19,30 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/todolists/%d/' % (list_.id,))
 
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/todolists/%d/' % (list_.id,))
+
+
+def json_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return JsonResponse(
+        {'result': 200, 'msg': [f"id:{item.id}, todoItem:{item.text}" for item in items]})
+
+
+def delete_item(request, list_id, item_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.filter(list=list_, id=item_id).delete()
+    return redirect('/todolists/%d/' % (list_.id,))
+
+
+def delete_list(request, list_id):
+    try:
+        list_ = List.objects.get(id=list_id)
+        Item.objects.filter(list=list_).delete()
+        List.objects.filter(id=list_id).delete()
+    finally:
+        return redirect('/')
